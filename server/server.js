b22026aa68cd7741be3cb5fc2626613caa411cfd
Siddharth Samir Khachane <khachane@usc.edu>
2025-03-27 19:49:23 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,16 +5,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://sidproject-41.wl.r.appspot.com");
-  res.header("Access-Control-Allow-Methods", "GET, POST");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-app.use(cors({
-  origin: "https://sidproject-41.wl.r.appspot.com"
-}));
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 const server = http.createServer(app);
