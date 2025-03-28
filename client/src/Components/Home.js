97028a6e6db798370e332540701d0d35a04f41cc
Siddharth Samir Khachane <khachane@usc.edu>
2025-03-27 19:50:16 -0700

    Update Home.js

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -13,7 +13,7 @@ const Home = () => {
     }
     
     try {
-      const response = await axios.post('https://collabcanvas-364v.onrender.com/api/rooms');
+      const response = await axios.post('https://skproject-a3.wl.r.appspot.com/api/rooms');
       const { roomId } = response.data;
       navigate(`/room/${roomId}?name=${userName}`);
     } catch (error) {
